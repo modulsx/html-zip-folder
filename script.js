96ad02fdfa3ce465948f3aa33473a5526605bf9a
Surya T <54438137+suryamodulus@users.noreply.github.com>
2022-09-07 23:14:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const zip = new JSZip();
+
+document.getElementById('filepicker').addEventListener(
+  'change',
+  async (event) => {
+    let output = document.getElementById('listing');
+    for (const file of event.target.files) {
+      let item = document.createElement('li');
+      item.textContent = file.webkitRelativePath;
+      output.appendChild(item);
+    }
+    await Promise.all(
+      [...event.target.files].map((file) => {
+        return addFileToZip(file, file.webkitRelativePath);
+      })
+    ).then(() => {
+      zip.generateAsync({ type: 'blob' }).then(function (content) {
+        saveAs(content, 'example.zip');
+      });
+    });
+  },
+  false
+);
+
+async function addFileToZip(file, filePath) {
+  return new Promise((resolve) => {
+    const fileReader = new FileReader();
+    fileReader.onload = (event) => {
+      zip.file(filePath, event.target.result);
+      resolve(true);
+    };
+    fileReader.readAsArrayBuffer(file);
+  });
+}
